23eac8acec54e64afbc15b740c70713a374ae3d8
Noor-Stylo <noor.stylo307@gmail.com>
2023-07-27 10:37:49 +0500

    homepage start

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <section>
+        <h1 className="text-center">Welcome to our website!</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Home
